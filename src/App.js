409f35ad8b05ae48244279b1d5f6852f224e5cb2
Weasel <cody.a.tracey@gmail.com>
2022-04-09 18:27:24 -0500

    added in basic react scaffolding, router, and bootstrap, starting to make my basic pages for testing the state hooks

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Pages
+import Home from './Components/Home/home';
+import Login from './Components/Login/login';
+import Landing from './Components/Landing/landing';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
