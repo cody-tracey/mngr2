374e31041d85f621ab7a93650649110ff5726ed7
Weasel <cody.a.tracey@gmail.com>
2022-06-07 18:46:22 -0500

    working on setting up the server and models for the app

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: `mongodb+srv://weasel:${process.env.MONGO_PW}@cluster0.9hpnu.mongodb.net/?retryWrites=true&w=majority`
+};
--- server.js
+++ server.js
@@ -1,12 +1,36 @@
+require('dotenv').config()
+
 const express = require('express'),
     port = process.env.PORT || 5000,
-    cors = require('cors');
+    cors = require('cors'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
 const APP = express();
 
+//Bodyparser Middleware
+APP.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+APP.use(bodyParser.json);
+
+//Good ol Cors
 APP.use(cors());
 
-APP.get('/api/greeting', (req, res) => {
-    res.status(200).json({ greeting: 'hello' });
-});
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+
+mongoose    
+    .connect(
+        db,
+        {useNewUrlParser: true}
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
 
 APP.listen(port, () => console.log(`Server running on ${port} => 🌎`));
